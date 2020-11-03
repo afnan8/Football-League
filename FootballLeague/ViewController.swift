87162c5c73ad2a79bf755e9c6ebe612b1294b649
afnan <afnan.h.mohamed@gmail.com>
2020-11-03 21:14:10 +0200

    Initial Commit

---
+++ FootballLeague/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FootballLeague
+//
+//  Created by Afnan on 11/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
