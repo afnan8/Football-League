6b59fff631392840a1a94aa9c82ca8806bd5fe40
afnan <afnan.h.mohamed@gmail.com>
2020-11-05 20:38:33 +0200

    load team data

--- FootballLeague/UI/Teams/View/TeamCollectionCell.swift
+++ FootballLeague/UI/Teams/View/TeamCollectionCell.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class TeamCollectionCell: UICollectionViewCell {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var colorsLabel: UILabel!
+    @IBOutlet weak var venueLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
+    func loadCellData(_ team: Team) {
+        nameLabel.text = team.name
+        colorsLabel.text = team.clubColors
+        venueLabel.text = "\(team.area.name) \(team.address)"
+    }
 }
